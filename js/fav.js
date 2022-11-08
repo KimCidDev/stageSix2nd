05cb93efbeeec0779e19dcf5b60e118a017806eb
Kim <cid.kim@gmail.com>
2022-11-08 02:55:14 -0300

    basic js modules and classes

--- js/fav.js
+++ js/fav.js
@@ -1,24 +1,18 @@
-export default class Wardrobe {
-  constructor(hanger, drawer, shelf, hidden) {
-    this.hanger = hanger
-    this.drawer = drawer
-    this.shelf = shelf
-    this.hidden = hidden
+class Favourites {
+  constructor(root) {
+    root = document.querySelector('root')
+  }
+}
+
+class FavouritesGitHub extends Favourites {
+  constructor(root) {
+    super(root)
   }
 
-  Inventory() {
-    console.log(
-      `Here's what I own: ${this.hanger}, ${this.drawer}, ${this.shelf} e ${this.hidden}`
-    )
-  }
-
-  WhatILikeMost() {
-    if (this.hanger === 'blue shirt') {
-      console.log('My beloved blue shirt')
-    } else if (this.hidden === 'weed') {
-      console.log('roll one, smoke one')
-    } else {
-      console.log('ok, time for some icecream')
-    }
+  removeItems() {
+    const tbody = document.querySelector('table tbody')
+    tbody.querySelectorAll('tr').forEach(tr => {
+      tr.remove()
+    })
   }
 }
--- js/index.js
+++ js/index.js
@@ -1,33 +1,3 @@
-// class Livro {
-//   constructor(nome, editora, paginas) {
-//     this.nome = nome
-//     this.editora = editora
-//     this.paginas = paginas
-//   }
+import FavouritesGitHub from './fav.js'
 
-//   anunciarTitulo() {
-//     console.log(`Título: ${this.nome}`)
-//   }
-
-//   descreverTitulo() {
-//     console.log(
-//       `${this.nome} é um livro da editora ${this.editora} e tem ${this.paginas} páginas`
-//     )
-//   }
-// }
-
-// const kim = new Livro('o vermelho e o negro', 'cia de letras', 192)
-
-// kim.descreverTitulo()
-// kim.anunciarTitulo()
-import Wardrobe from './fav.js'
-
-const kimswardrobe = new Wardrobe(
-  'blue shirt',
-  'just socks',
-  'weird symbols',
-  'weed'
-)
-
-kimswardrobe.Inventory()
-kimswardrobe.WhatILikeMost()
+const dataEntry = new FavouritesGitHub('#app')
